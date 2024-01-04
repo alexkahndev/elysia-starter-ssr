64f255ad033a18446e39c35168e8f707f655b746
Andy Cordero <corderoandy5@gmail.com>
2024-01-04 17:00:32 -0500

    added branch for andy

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,6 +2,7 @@ const Landing = () => {
   return (
     <div>
       <h1 className="text-4xl font-bold">Landing</h1>
+      <h1 className="text-4xl font-bold">Andy Cordero</h1>
     </div>
   );
 };
