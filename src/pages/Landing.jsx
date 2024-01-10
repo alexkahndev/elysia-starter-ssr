75cdf59ff393025347129cc255f2128a3b39176e
raniya <114628422+RaniyaR@users.noreply.github.com>
2024-01-10 00:20:37 -0500

    form component - styling still needed

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,11 @@
+import React from "react";
+import Form from "../components/Form";
+
 const Landing = () => {
   return (
     <div>
       <h1 className="text-4xl font-bold">Landing</h1>
+      <Form />
     </div>
   );
 };
