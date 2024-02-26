d901a6eaa42261a67c13c59fdba229d64702fd0e
Andy Cordero <corderoandy5@gmail.com>
2024-02-25 23:01:28 -0500

    fixed the styling from the navbar component

--- src/components/utils/Navbar.tsx
+++ src/components/utils/Navbar.tsx
@@ -5,27 +5,17 @@ export const Navbar = () => {
         <a href="/">
           <img src="/assets/brand_logo.svg" alt="Awesome.Social Logo" />
         </a>
-      </header>
-      <main>
-        <ul>
-          <li>
-            <a href="/">About</a>
-          </li>
-          <li>
-            <a href="/">Service</a>
-          </li>
-          <li>
-            <a href="/" className="Button">
-              Login
-            </a>
-          </li>
-          <li>
-            <a href="/" className="Button">
-              Sign Up
-            </a>
-          </li>
-        </ul>
-      </main>
-    </nav>
+        <aside>
+          <a href="/">About</a>
+          <a href="/">Service</a>
+          <a href="/" className="Button">
+            Login
+          </a>
+          <a href="/" className="Button">
+            Sign Up
+          </a>
+        </aside>
+      </nav>
+    </header>
   );
 };
