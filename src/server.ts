febbb6bd96d55c9c472f2c84b8b0437d13047c2e
Andy Cordero <corderoandy5@gmail.com>
2024-03-13 23:07:21 -0500

    Created FAQ page and Fix position of Sidebar

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import History from "./pages/History";
+import FAQ from "./pages/FAQ";
 import { Services } from "./pages/Services";
 
 const host = Bun.env.HOST || "localhost";
@@ -20,7 +21,8 @@ await Bun.build({
     "./src/indexes/AboutIndex.tsx",
     "./src/indexes/ServicesIndex.tsx",
     "./src/indexes/ContactIndex.tsx",
-    "./src/indexes/HistoryIndex.tsx"
+    "./src/indexes/HistoryIndex.tsx",
+    "./src/indexes/FAQIndex.tsx",
   ],
   outdir: "./build",
   minify: true,
@@ -60,6 +62,7 @@ export const server = new Elysia()
   .get("/about", () => handleRequest(About, "/AboutIndex.js"))
   .get("/services", () => handleRequest(Services, "/ServicesIndex.js"))
   .get("/history", () => handleRequest(History, "/HistoryIndex.js"))
+  .get("/faqs", () => handleRequest(FAQ, "/FAQIndex.js"))
   .post("/create-user", ({ body }) => createUser(body), {
     body: t.Object({
       firstName: t.String(),
@@ -80,7 +83,7 @@ export const server = new Elysia()
   //API Endpoints for services
 
   //Retrieve list of all services
- .get("/services/list", async () => getServices())
+  .get("/services/list", async () => getServices())
 
   //Retrieve a service based on its ID
   .get("/services/:id", async (req) => { 
